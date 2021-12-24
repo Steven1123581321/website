b1d4be64f63aef977c3e337f4a34f19bde485573
Slimstock_SP <stevenpauly1988@hotmail.com>
2021-12-24 11:20:53 +0100

    added cv model

--- cv/admin.py
+++ cv/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from cv.models import Cv2
+
+
+class CvAdmin(admin.ModelAdmin):
+    fields = ["welcome_title", "welcome_text"]
+
+
+admin.site.register(Cv2, CvAdmin)
