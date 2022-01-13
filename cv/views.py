a3f4a162c6faddc4251c0080e46ba8aef4aff4c1
Slimstock_SP <stevenpauly1988@hotmail.com>
2022-01-13 19:08:06 +0100

    cleaned up html templates for cv and blog

--- cv/views.py
+++ cv/views.py
@@ -5,14 +5,6 @@ from cv.models import Cv2
 
 # main index view
 def index(request):
-    # file_path = "./cv/static/configuration/configuration.json"
-    # with open(file_path) as f:
-    #     configuration = json.loads(f.read())
-    # return render(request, "cv_index.html", configuration)
-    file_path = "./cv/configuration/configuration.json"
-    with open(file_path) as f:
-        configuration = json.loads(f.read())
     cv = Cv2.objects.all()
     context = {"cv": cv}
-    context.update(configuration)
-    return render(request, "cv_index_2.html", context)
+    return render(request, "cv_index.html", context)
